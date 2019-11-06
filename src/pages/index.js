d411dc4dad347cd4c8c8cef88c2eba07955c53fd
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-06 15:04:04 -0500

    Added GraphQL in the Index page Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,33 @@
 import React from "react"
+import { graphql } from 'gatsby'
 import PrimaryLayout from '../layouts/PrimaryLayout'
 import Post from '../components/Post'
 
-export default () => (
-    
-    <PrimaryLayout column='col-xs-6'>
-        <div className="container">
-            <div className="row justify-content-md-center">
-                <div className="col-xs-6"> 
-                    <Post 
-                        title="First Blogpost" 
-                        excerpt="Gatsby is a blazing fast modern site generator for React. The quick start is intended for intermediate to advanced developers. For a gentler intro to Gatsby, head to our tutorial!" 
-                    />
-                    <Post 
-                        title="First Blogpost" 
-                        excerpt="Gatsby is a blazing fast modern site generator for React. The quick start is intended for intermediate to advanced developers. For a gentler intro to Gatsby, head to our tutorial!" 
-                    />
-                    <Post 
-                        title="First Blogpost" 
-                        excerpt="Gatsby is a blazing fast modern site generator for React. The quick start is intended for intermediate to advanced developers. For a gentler intro to Gatsby, head to our tutorial!" 
-                    />
-                </div>
-            </div>
-        </div>
-    </PrimaryLayout>
-        
-);
+export default ({data}) => {// The data returned by the graphql query, in the props, is obtanied directly by destructurizing the props
+    console.log(data); // Console log the data
+    return( // Render the component
+        <PrimaryLayout column='col-xs-6'>
+            <Post 
+                title="First Blogpost" 
+                excerpt="Gatsby is a blazing fast modern site generator for React. The quick start is intended for intermediate to advanced developers. For a gentler intro to Gatsby, head to our tutorial!" 
+            />
+        </PrimaryLayout>        
+    );
+}
+// This query will be added to the props of the component
+export const query = graphql`
+{
+    allMarkdownRemark{
+      nodes{
+        frontmatter{
+          title
+          date
+          keywords
+          image
+        }
+        excerpt
+        html
+      }
+    }
+  }
+`
