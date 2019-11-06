caad119e80bf74fd1c82080a03a6f34c6d0054cc
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-06 12:45:23 -0500

    Added Gatsby Filesystem Plugin adn Transformer Remark Plugin

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { Link } from 'gatsby';
 const Header = (props) => {
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">Gatsby Blog</Navbar.Brand>
+            <Navbar.Brand href="#home">Gatsby-Blog</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
